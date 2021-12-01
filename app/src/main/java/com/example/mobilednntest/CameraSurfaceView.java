e3d1d28b5fa4c62e23fde8db8121f6eb7216c255
Jaekyung-Cho <jackyoung96@snu.ac.kr>
2021-12-01 23:06:44 +0900

    Android app 버전 1 1. Depth map 기능 2. Classification 기능 3. Auto focusing 기능 구현된 첫번째 App

---
+++ app/src/main/java/com/example/mobilednntest/CameraSurfaceView.java
@@ -0,0 +1,159 @@
+package com.example.mobilednntest;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.ImageFormat;
+import android.graphics.Matrix;
+import android.graphics.Rect;
+import android.graphics.YuvImage;
+import android.hardware.Camera;
+import android.hardware.camera2.CameraDevice;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.ImageView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+public class CameraSurfaceView extends SurfaceView implements SurfaceHolder.Callback, Camera.PreviewCallback {
+    private CameraDevice camera;
+    private SurfaceView mCameraView;
+    private SurfaceHolder mCameraHolder;
+    private Camera mCamera;
+    private boolean recording = false;
+    private Bitmap bitmap;
+    private ImageView resultimageView;
+    private MainActivity activity;
+
+    public CameraSurfaceView(Context context) {
+        super(context);
+        activity = (MainActivity) context;
+    }
+
+    @Override
+    public void onPreviewFrame(byte[] data, Camera camera){
+        try {
+            Camera.Parameters parameters = camera.getParameters();
+            int imageFormat = parameters.getPreviewFormat();
+            Bitmap bitmap = null;
+
+            if (imageFormat == ImageFormat.NV21) {
+                int w = parameters.getPreviewSize().width;
+                int h = parameters.getPreviewSize().height;
+
+                YuvImage yuvImage = new YuvImage(data, imageFormat, w, h, null);
+                Rect rect = new Rect(0, 0, w, h);
+                ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                yuvImage.compressToJpeg(rect, 50, outputStream);
+                bitmap = BitmapFactory.decodeByteArray(outputStream.toByteArray(), 0, outputStream.size());
+            }
+            else if (imageFormat == ImageFormat.JPEG || imageFormat == ImageFormat.RGB_565) {
+                bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+            }
+
+            if(bitmap != null) {
+                Matrix rotateMatrix = new Matrix();
+                rotateMatrix.postRotate(90);
+                Bitmap finalBitmap = Bitmap.createBitmap(bitmap, 0,0, bitmap.getWidth(), bitmap.getHeight(), rotateMatrix, false);
+                activity.runOnUiThread(new Runnable(){
+                    @Override
+                    public void run(){
+                        activity.depthShow(finalBitmap);
+//                        ((ImageView) findViewById(R.id.result_image)).setImageBitmap(finalBitmap);
+                    }
+                });
+            }
+        } catch(Exception e){
+
+        }
+    }
+
+    // Camera 기능
+    public void init(SurfaceView surfaceView){
+        System.out.println("init");
+        mCamera = Camera.open();
+        Camera.Parameters parameters = mCamera.getParameters();
+        List<String> focusModes = parameters.getSupportedFocusModes();
+        if (focusModes.contains(Camera.Parameters.FOCUS_MODE_AUTO)) {
+            parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
+        }
+        parameters.setPreviewSize(640, 360);
+//        parameters.setPreviewFpsRange(10000,10000);
+        mCamera.setParameters(parameters);
+        mCamera.setDisplayOrientation(90);
+
+        // surfaceview setting
+        mCameraView = surfaceView;
+        mCameraHolder = mCameraView.getHolder();
+        mCameraHolder.addCallback(this);
+        mCameraHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        resultimageView = (ImageView) findViewById(R.id.result_image);
+    }
+
+    public void autofocus(){
+        System.out.println("autofocus");
+        mCamera.autoFocus(new Camera.AutoFocusCallback() {
+            @Override
+            public void onAutoFocus(boolean success, Camera camera) {
+
+            }
+        });
+    }
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        try {
+            System.out.println("surfaceCreated");
+            if (mCamera == null) {
+                mCamera.reconnect();
+                mCamera.setPreviewDisplay(holder);
+                mCamera.startPreview();
+            }
+        } catch (IOException e) {
+        }
+    }
+
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+        // View 가 존재하지 않을 때
+        System.out.println("surfaceChanged");
+        if (mCameraHolder.getSurface() == null) {
+            return;
+        }
+
+        // 작업을 위해 잠시 멈춘다
+        try {
+            mCamera.stopPreview();
+        } catch (Exception e) {
+            // 에러가 나더라도 무시한다.
+        }
+
+
+
+        // View 를 재생성한다.
+        try {
+            // 카메라 설정을 다시 한다.
+            Camera.Parameters parameters = mCamera.getParameters();
+            List<String> focusModes = parameters.getSupportedFocusModes();
+            if (focusModes.contains(Camera.Parameters.FOCUS_MODE_AUTO)) {
+                parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
+            }
+//        parameters.setPreviewFpsRange(10000,10000);
+            mCamera.setParameters(parameters);
+            mCamera.setPreviewDisplay(mCameraHolder);
+            mCamera.startPreview();
+            mCamera.setPreviewCallback(this);
+        } catch (Exception e) {
+        }
+    }
+
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        if (mCamera != null) {
+            mCamera.stopPreview();
+            mCamera.release();
+            mCamera = null;
+            System.out.println("release");
+        }
+    }
+}
---
+++ app/src/main/java/com/example/mobilednntest/MainActivity.java
@@ -0,0 +1,291 @@
+package com.example.mobilednntest;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.provider.Settings;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import org.pytorch.IValue;
+import org.pytorch.Module;
+import org.pytorch.Tensor;
+import org.pytorch.torchvision.TensorImageUtils;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+
+public class MainActivity extends AppCompatActivity{
+    private static int RESULT_LOAD_IMAGE = 1;
+    CameraSurfaceView cameraSurfaceView;
+    private static final int SINGLE_PERMISSION = 1004; //권한 변수
+    Module module_detection = null; // detection 모델
+    Module module_depth = null; // depth 모델
+    private Bitmap raw_bitmap;
+
+    //
+    public void depthShow(Bitmap rawbitmap){
+        if (module_depth != null) {
+            ((TextView) findViewById(R.id.depth_text)).setText("");
+            raw_bitmap = rawbitmap;
+            Bitmap bitmap = null;
+            int width = rawbitmap.getWidth();
+            int height = rawbitmap.getHeight();
+            bitmap = Bitmap.createBitmap(rawbitmap, 0, (height-width)/2, width, width);
+            bitmap = Bitmap.createScaledBitmap(bitmap, 224, 224, true);
+            //Input Tensor
+            final Tensor input = TensorImageUtils.bitmapToFloat32Tensor(
+                    bitmap,
+                    TensorImageUtils.TORCHVISION_NORM_MEAN_RGB,
+                    TensorImageUtils.TORCHVISION_NORM_STD_RGB
+            );
+
+            //Calling the forward of the model to run our input
+            final Tensor output = module_depth.forward(IValue.from(input)).toTensor();
+            final float[] deptharray = output.getDataAsFloatArray();
+            Bitmap depthbitmap = arrayFlotToBitmap(deptharray, 224, 224);
+            final Bitmap finaldepthbitmap = Bitmap.createScaledBitmap(depthbitmap, width, width, true);
+
+            //
+            ((ImageView) findViewById(R.id.result_image)).setImageBitmap(finaldepthbitmap);
+        }
+        else{
+            ((TextView) findViewById(R.id.depth_text)).setText("Please upload model");
+        }
+    }
+    private Bitmap arrayFlotToBitmap(float[] floatArray,int width,int height){
+
+        byte alpha = (byte) 255 ;
+
+        Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888) ;
+
+        ByteBuffer byteBuffer = ByteBuffer.allocate(width*height*4) ;
+
+        int i = 0 ;
+        while (i<floatArray.length){
+            byte temValue = (byte) Math.floor(floatArray[i]*255);
+            byteBuffer.put(4*i, temValue) ;
+            byteBuffer.put(4*i+1, temValue) ;
+            byteBuffer.put(4*i+2, temValue) ;
+            byteBuffer.put(4*i+3, alpha) ;
+            i++ ;
+        }
+        bmp.copyPixelsFromBuffer(byteBuffer) ;
+        return bmp ;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) { // on수Create 가 가장 먼저 실행되는 함
+        super.onCreate(savedInstanceState);
+        //권한이 있는지 확인
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {//권한없음
+            //권한 요청 코드
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA}, SINGLE_PERMISSION);
+        } else {//권한있음
+
+            /*..권한이 있는경우 실행할 코드....*/
+            setContentView(R.layout.activity_main);
+
+            Button buttonLoadImage = (Button) findViewById(R.id.button);
+            Button detectButton = (Button) findViewById(R.id.detect);
+            Button autofocusButton = (Button) findViewById(R.id.autofocus);
+
+            SurfaceView mCameraView = (SurfaceView) findViewById(R.id.cameraView);
+            cameraSurfaceView = new CameraSurfaceView(this);
+            cameraSurfaceView.init(mCameraView);
+
+            buttonLoadImage.setOnClickListener(new View.OnClickListener() {
+
+
+                @Override
+                public void onClick(View arg0) {
+                    try {
+                        System.out.println("detection model load");
+                        module_detection = Module.load(fetchModelFile(MainActivity.this, "resnet18_traced.pt"));
+                        System.out.println("depth model load");
+                        module_depth = Module.load(fetchModelFile(MainActivity.this, "project_fastdepth_trace_cpu.pt"));
+                        System.out.println("model load complete");
+                    } catch (IOException e) {
+                        finish();
+                    }
+                }
+            });
+
+            detectButton.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View arg0) {
+
+                    if (module_detection != null) {
+                        Bitmap bitmap = null;
+
+                        //Read the image as Bitmap
+                        bitmap = raw_bitmap;
+                        //Here we reshape the image into 400*400
+                        bitmap = Bitmap.createScaledBitmap(bitmap, 224, 224, true);
+
+                        //Input Tensor
+                        final Tensor input = TensorImageUtils.bitmapToFloat32Tensor(
+                                bitmap,
+                                TensorImageUtils.TORCHVISION_NORM_MEAN_RGB,
+                                TensorImageUtils.TORCHVISION_NORM_STD_RGB
+                        );
+                        //Calling the forward of the model to run our input
+                        final Tensor output = module_detection.forward(IValue.from(input)).toTensor();
+
+                        final float[] score_arr = output.getDataAsFloatArray();
+
+                        // Fetch the index of the value with maximum score
+                        float max_score = -Float.MAX_VALUE;
+                        int ms_ix = -1;
+                        for (int i = 0; i < score_arr.length; i++) {
+                            if (score_arr[i] > max_score) {
+                                max_score = score_arr[i];
+                                ms_ix = i;
+                            }
+                        }
+                        //Fetching the name from the list based on the index
+                        String detected_class = ModelClasses.MODEL_CLASSES[ms_ix];
+
+                        //Writing the detected class in to the text view of the layout
+                        TextView textView = findViewById(R.id.result_text);
+                        textView.setText(detected_class);
+                    }
+                    else{
+                        TextView textView = findViewById(R.id.result_text);
+                        textView.setText("Please upload model");
+                    }
+                }
+            });
+
+            autofocusButton.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View arg0) {
+                    cameraSurfaceView.autofocus();
+                }
+            });
+        }
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //This functions return the selected image from gallery
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+//            // image 위치에 뿌려주는 것
+//            ImageView imageView = (ImageView) findViewById(R.id.image);
+//            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+//
+//            //Setting the URI so we can read the Bitmap from the image
+//            imageView.setImageURI(null);
+//            imageView.setImageURI(selectedImage);
+
+            // 일단 같은 이미지 뿌려줌
+            ImageView resultimageView = (ImageView) findViewById(R.id.result_image);
+            Bitmap bitmap = BitmapFactory.decodeFile(picturePath);
+            resultimageView.setImageBitmap(bitmap);
+            System.out.println(bitmap.getHeight());
+            System.out.println(bitmap.getWidth());
+            System.out.println(bitmap.getConfig());
+
+            //Setting the URI so we can read the Bitmap from the image
+            resultimageView.setImageURI(null);
+            resultimageView.setImageURI(selectedImage);
+
+        }
+
+
+    }
+
+    // Pytorch model fetch
+    public static String fetchModelFile(Context context, String modelName) throws IOException {
+        File file = new File(context.getFilesDir(), modelName);
+        if (file.exists() && file.length() > 0) {
+            return file.getAbsolutePath();
+        }
+
+        try (InputStream is = context.getAssets().open(modelName)) {
+            try (OutputStream os = new FileOutputStream(file)) {
+                byte[] buffer = new byte[4 * 1024];
+                int read;
+                while ((read = is.read(buffer)) != -1) {
+                    os.write(buffer, 0, read);
+                }
+                os.flush();
+            }
+            return file.getAbsolutePath();
+        }
+    }
+
+    //권한 요청에 대한 결과 처리
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case SINGLE_PERMISSION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    /*권한이 있는경우 실행할 코드....*/
+                } else {
+                    // 하나라도 거부한다면.
+                    AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+                    alertDialog.setTitle("앱 권한");
+                    alertDialog.setMessage("해당 앱의 원할한 기능을 이용하시려면 애플리케이션 정보>권한> 에서 모든 권한을 허용해 주십시오");
+                    // 권한설정 클릭시 이벤트 발생
+                    alertDialog.setPositiveButton("권한설정",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).setData(Uri.parse("package:" + getApplicationContext().getPackageName()));
+                                    startActivity(intent);
+                                    dialog.cancel();
+                                }
+                            });
+                    //취소
+                    alertDialog.setNegativeButton("취소",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.cancel();
+                                }
+                            });
+                    alertDialog.show();
+                }
+                return;
+        }
+
+    }
+}
